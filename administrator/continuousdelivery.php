bac9627e95e3d5447786612803399d8fc0e47a39
Clay Freeman <git@clayfreeman.com>
2017-05-30 11:10:01 -0500

    Update continuousdelivery.php

--- administrator/continuousdelivery.php
+++ administrator/continuousdelivery.php
@@ -1,3 +1,10 @@
 <?php
+  /**
+   * Joomla's designated entrypoint into this component.
+   *
+   * @copyright  Copyright 2016 Clay Freeman. All rights reserved.
+   * @license    GNU Lesser General Public License v3 (LGPL-3.0).
+   */
+
   // Redirect to the administrator site root
   JFactory::getApplication()->redirect(JRoute::_('/administrator'));
