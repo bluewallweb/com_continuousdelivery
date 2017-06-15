2635bace92c956fa81f43bf891ee397f526b0580
Clay Freeman <git@clayfreeman.com>
2017-06-15 11:59:20 -0500

    Create continuousdelivery.php

--- admin/continuousdelivery.php
+++ admin/continuousdelivery.php
@@ -6,7 +6,5 @@
    * @license    GNU Lesser General Public License v3 (LGPL-3.0).
    */
 
-  (function() {
-    // Redirect to the administrator site root
-    JFactory::getApplication()->redirect(JRoute::_('/administrator'));
-  })();
+  // Redirect to the administrator site root
+  JFactory::getApplication()->redirect(JRoute::_('/administrator'));
